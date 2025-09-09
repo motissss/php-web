cde41cf1852d3a77c098de7478cc5c2ca158c0ca
unknown <john@same.com>
2025-09-09 11:39:09 +0300

    meow

--- people.php
+++ people.php
@@ -1,38 +1,49 @@
-<?php 
-$people = array(
-    ["name"=>"Andris",
-    "email" => "Andris@gmail.com",
-    "Languges" => "C++"
-],
-    ["name"=>"Kristers",
-    "email" => "Kristers@gmail.com",
-    "Languges" => "Php"
-],
-    [
-    "name"=>"Pēteris",
-    "email" => "Pjord@gmail.com",
-    "Languges" => "C#"
-    ]
-)
+<?php
+class Person {
+    public $name;
+    public $email;
+    public $language;
+    
+    public function __construct($name, $email, $language) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->language = $language;
+    }
+
+    public function get_name() {
+        return $this->name;
+    }
+
+    public function get_email() {
+        return $this->email;
+    }
+
+    public function get_language() {
+        return $this->language;
+    }
+}
+$people = [
+    new Person("Andris", "andris@gmail.com", "PHP"),
+    new Person("Kristers", "kristers@gmail.com", "JavaScript"),
+    new Person("Pēteris", "peteris@gmail.com", "Python")
+];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Saraksts ar personām</title>
 </head>
 <body>
     <h1>Saraksts ar personām:</h1>
     <ul>
         <?php foreach ($people as $person): ?>
             <li>
-                <strong>Vārds:</strong> <?= htmlspecialchars($person['name']) ?><br>
-                <strong>E-pasts:</strong> <?= htmlspecialchars($person['email']) ?><br>
-                <strong>Valoda:</strong> <?= htmlspecialchars($person['Languges']) ?>
+                <strong>Vārds:</strong> <?= htmlspecialchars($person->get_name()) ?><br>
+                <strong>E-pasts:</strong> <?= htmlspecialchars($person->get_email()) ?><br>
+                <strong>Valoda:</strong> <?= htmlspecialchars($person->get_language()) ?>
             </li>
         <?php endforeach; ?>
     </ul>
-    
 </body>
 </html>
